cc208b63bcb5243610edfc1a6b9992363e24764f
dmitriy <dmvasyuk@gmail.com>
2018-05-26 14:38:37 +0300

    Added messaging server

---
+++ README.md
@@ -0,0 +1,18 @@
+## About
+
+Real-time file stored server
+
+## Getting Started
+
+1. Make sure you have [NodeJS](https://nodejs.org/) and [npm](https://www.npmjs.com/) installed.
+2. Install your dependencies
+
+    ```
+    cd path/to/local-messaging; npm install
+    ```
+
+3. Start your app
+
+    ```
+    npm start
+    ```
---
+++ src/services/messages/messages.hooks.js
@@ -0,0 +1,35 @@
+const { authenticate } = require('@feathersjs/authentication').hooks;
+
+module.exports = {
+  before: {
+    // uncomment to hide from unauthorised users
+    // all: [ authenticate('jwt') ],
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
